fccba3fe498488f732b395097e5a3c43d9fa216e
Tran Duy Anh <anhdtran@gmail.com>
2018-10-24 22:50:24 +0800

    something

--- scripts.js
+++ scripts.js
@@ -1,25 +1,16 @@
-// $(function () {
-//     $.ajax({
-//         url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback',
-//         dataType: 'jsonp'
-//     });
-
-//     setInterval(function () {
-//         $('.main').fadeOut("slow", function () {
-//             $.ajax({
-//                 url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback',
-//                 dataType: 'jsonp'
-//             });
-//         })
-//     }, 10000);
-// });
-
-// function mycallback(e) {
-//     console.log(e)
-//     $(".main div blockquote").empty();
-//     $(".main div blockquote").append(e[0].content);
-//     $(".mb-author").text("~" + e[0].title);
-//     $(".main").fadeIn("slow", function () {
-//         console.log("quote successfully loaded!")
-//     });
-// }
+$(function(){
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop(); // how many pixels you've scrolled
+        var os = $('#about_me').offset().top; // pixels to the top of div1
+        var ht = $('#about_me').height(); // height of div1 in pixels
+        // if you've scrolled further than the top of div1 plus it's height
+        // change the color. either by adding a class or setting a css property
+        if(scroll > os + ht){
+            $('a[href="#second_middle"]').addClass('current');
+            $('a[href="#about_me"]').removeClass('current');
+        }else{
+            $('a[href="#second_middle"]').removeClass('current');
+            $('a[href="#about_me"]').addClass('current');
+        }
+    });
+});
