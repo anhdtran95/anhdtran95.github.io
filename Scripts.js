632fd70dcc824392291c4e752a42370bb764e176
Tran Duy Anh <anhdtran@gmail.com>
2018-10-24 08:46:27 +0800

    clean up

--- Scripts.js
+++ Scripts.js
@@ -1,1 +1,25 @@
+$(function () {
+    $.ajax({
+        url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback',
+        dataType: 'jsonp'
+    });
 
+    setInterval(function () {
+        $('.main').fadeOut("slow", function () {
+            $.ajax({
+                url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback',
+                dataType: 'jsonp'
+            });
+        })
+    }, 10000);
+});
+
+function mycallback(e) {
+    console.log(e)
+    $(".main div blockquote").empty();
+    $(".main div blockquote").append(e[0].content);
+    $(".mb-author").text("~" + e[0].title);
+    $(".main").fadeIn("slow", function () {
+        console.log("quote successfully loaded!")
+    });
+}
